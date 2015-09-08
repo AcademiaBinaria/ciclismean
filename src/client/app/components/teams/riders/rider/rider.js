247042e2fae607999ef808a3418d0b2374313b3d
antonioferngarcia <antonio.ferngarcia@gmail.com>
2015-09-08 13:34:54 +0200

    Página de rider

--- src/client/app/components/teams/riders/rider/rider.js
+++ src/client/app/components/teams/riders/rider/rider.js
@@ -4,29 +4,68 @@
         .module('rider', ['ui.router'])
         .config(config)
         .directive('rider', directive)
+        .factory('riderLogicService', riderLogicService);
 
     function config($stateProvider) {
         $stateProvider
             .state('rider', {
-                url: 'equipo/:teamId/:year/:riderId',
+                url: '/equipos/:teamId/:year/:riderId',
                 template: '<rider></rider>'
             });
     }
 
     function directive() {
-        function controller() {
-            var vm = this;
-        }
 
         return {
-            templateUrl: 'client/app/components/teams/riders/rider/rider.html',
+            templateUrl: 'app/components/teams/riders/rider/rider.html',
             controller: controller,
             controllerAs: "vm",
             bindToController: true
         }
     }
 
-    function controller() {
+    function controller($stateParams, ridersDataService, riderLogicService) {
         var vm = this;
+        vm.riderId = $stateParams.riderId;
+
+        init();
+
+        function init() {
+            ridersDataService.gettingRiders({
+                limit: 1,
+                q: 'safe_name:' + vm.riderId
+            }).then(function (riders) {
+                vm.rider = riders[0];
+                vm.imageUrl = riderLogicService.getRiderImageUrl(vm.rider);
+                vm.rider.age = riderLogicService.getRiderAge(vm.rider.dob);
+                vm.rider.flag = riderLogicService.getRiderFlag(vm.rider.country);
+            });
+        }
     }
+
+    function riderLogicService() {
+        var factory = {};
+
+        factory.getRiderFlag = function (country) {
+            return "assets/images/flags/" + country + ".png";
+        }
+
+        factory.getRiderAge = function (dob) {
+            var dateOfBirth = new Date(dob);
+            var ageDifMs = Date.now() - dateOfBirth.getTime();
+            var ageDate = new Date(ageDifMs);
+            return (ageDate.getUTCFullYear() - 1970);
+        }
+
+        factory.getRiderImageUrl = function (rider) {
+            var year = new Date().getFullYear();
+            var team = rider.safe_name_team;
+            var rider = rider.safe_name;
+            return "assets/images/riders_img/" + year + "/" + team + "/" + rider + ".jpg";
+        }
+
+        return factory;
+    }
+
+
 })();
--- src/server/lib/routes/api/ridersApi.js
+++ src/server/lib/routes/api/ridersApi.js
@@ -7,21 +7,28 @@ var router = express.Router({
 });
 
 var schema = {
-        id: "ridersDetails",
-        type: "object",
-        properties: {
-            _id: { type: "string" },
-            safe_name: { type: "string" },
-            team: { type: "string" },
-            safe_name_team: { type: "string" },
-            country: { type: "string" }
+    id: "ridersDetails",
+    type: "object",
+    properties: {
+        _id: {
+            type: "string"
         },
-        required: ["_id", "safe_name", "team", "safe_name_team","country"]
-    }
-crudApi(router,ridersData, schema);
-
+        safe_name: {
+            type: "string"
+        },
+        team: {
+            type: "string"
+        },
+        safe_name_team: {
+            type: "string"
+        },
+        country: {
+            type: "string"
+        }
+    },
+    required: ["_id", "safe_name", "team", "safe_name_team", "country"]
+}
+crudApi(router, ridersData, schema);
 
 
 module.exports = router;
-
-
