eb0c0b4b25cb444e3d60707cbc997723ca60fbb3
antonioferngarcia <antonio.ferngarcia@gmail.com>
2015-11-04 11:38:47 +0100

    white list

--- src/client/app/_common/app.js
+++ src/client/app/_common/app.js
@@ -4,4 +4,11 @@ angular
     .module('template', ['ui.router', 'ui.bootstrap', 'ngStorage', 'main', 'navbar', 'footer', 'user', 'teams', 'competitions', 'util', 'dashboard', 'riderForm', 'teamForm'])
     .constant('settings', {
         urlBase: 'http://localhost:3030'
+    })
+    .config(function ($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+            "self",
+            "http://localhost:3000/**",
+            "http://46.101.149.113/**"
+        ]);
     });
