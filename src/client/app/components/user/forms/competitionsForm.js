0e6480e33fa49daa0fcb0347c7447d96cd0aa16b
antonioferngarcia <antonio.ferngarcia@gmail.com>
2015-11-19 14:01:10 +0100

    First aproach to edit stage victories and the data update on riders and competitions collections

--- src/client/app/components/user/forms/competitionsForm.js
+++ src/client/app/components/user/forms/competitionsForm.js
@@ -1,97 +1,182 @@
 "use strict";
 (function () {
-    angular
-        .module('competitionForm', ['ui.router', 'formMessages'])
-        .config(config)
-        .directive('competitionForm', directive)
+	angular
+		.module('competitionForm', ['ui.router', 'formMessages'])
+		.config(config)
+		.directive('competitionForm', directive)
 
 
-    function config($stateProvider) {
-        $stateProvider
-            .state('edit-competitons', {
-                url: '/forms/competitions',
-                template: '<competition-form></competition-form>'
-            });
-    }
+	function config($stateProvider) {
+		$stateProvider
+			.state('edit-competitons', {
+				url: '/forms/competitions',
+				template: '<competition-form></competition-form>'
+			});
+	}
 
-    function directive() {
-        return {
-            templateUrl: 'app/components/user/forms/competitionsForm.html',
-            controller: controller,
-            controllerAs: "vm",
-            bindToController: true
-        }
-    }
+	function directive() {
+		return {
+			templateUrl: 'app/components/user/forms/competitionsForm.html',
+			controller: controller,
+			controllerAs: "vm",
+			bindToController: true
+		}
+	}
 
-    function controller(UtilService, $state, $rootScope, competitionDataService, $stateParams, $scope, $uibModal) {
-        var vm = this;
-        var limit = 100;
-        vm.has_error = UtilService.has_error;
-        vm.showForm = false;
-        vm.team_id = "";
-        vm.showMessage = false;
-        if (!UtilService.isLogged())
-            $state.go('login');
+	function controller(UtilService, $state, competitionDataService, $scope, ridersDataService) {
+		var vm = this;
+		var limit = 100;
+		vm.has_error = UtilService.has_error;
+		vm.showForm = false;
+		vm.team_id = "";
+		vm.showMessage = false;
+		if (!UtilService.isLogged())
+			$state.go('login');
 
-        init();
+		init();
 
-        function init() {
-            competitionDataService.gettingCompetitions({
-                limit: limit
-            }).then(function (data) {
-                vm.competitions = data;
-            });
-        }
+		function init() {
+			competitionDataService.gettingCompetitions({
+				limit: limit
+			}).then(function (data) {
+				vm.competitions = data;
+			});
+		}
+
+		function addVictory(winner, season, competition) {
+			season = parseInt(season);
+			vm.riders.forEach(function (rider) {
+				if (rider._id == winner) {
+					var seasonFound = false;
+					var competitionFound = false;
+					rider.seasons.forEach(function (year, yearPos) {
+						if (year.year == season) {
+							var seasonFound = true;
+							year.palmares.forEach(function (race, racePos) {
+								if (race.competition == competition) {
+									var competitionFound = true;
+									vm.rider = new ridersDataService.rider();
+									for (var propertyName in rider) {
+										vm.rider[propertyName] = rider[propertyName];
+									}
+									vm.rider.seasons[yearPos].palmares[racePos].victories += 1;
+								}
+							});
+							if (!competitionFound) {
+								vm.rider = new ridersDataService.rider();
+								for (var propertyName in rider) {
+									vm.rider[propertyName] = rider[propertyName];
+								}
+								vm.rider.seasons[yearPos].palmares.push({
+									competition: competition,
+									position: null,
+									victories: 1
+								});
+							}
+						}
+					});
+					if (!seasonFound) {
+						vm.rider = new ridersDataService.rider();
+						for (var propertyName in rider) {
+							vm.rider[propertyName] = rider[propertyName];
+						}
+						vm.rider.seasons.push({
+							year: season,
+							team: rider.team,
+							palmares: [{
+								competition: competition,
+								position: null,
+								victories: 1
+							}]
+						})
+					}
+					console.log(vm.rider);
+				}
+			});
+		}
+
+		function findWinnersChange() {
+			vm.competition.seasons.forEach(function (season, seasonIndex) {
+				season.stages.forEach(function (stage, stageIndex) {
+					if (stage.winner != "-") {
+						if (vm.competitionNoEdited.seasons[seasonIndex].stages[stageIndex].winner != stage.winner) {
+							console.log("Añadimos victoria: " + stage.winner);
+							//vm.rider = new ridersDataService.rider();
+							addVictory(stage.winner, season.year, vm.competition._id);
+							if (vm.competitionNoEdited.seasons[seasonIndex].stages[stageIndex].winner != "-") {
+								console.log("Quitamos victoria: " + vm.competitionNoEdited[seasonIndex].stages[stageIndex].winner);
+							}
+						}
+					}
+				});
+			});
+		}
+
+		function findRiders() {
+			ridersDataService.gettingRiders({
+					limit: 2000,
+					skip: 0,
+					sort: "_id",
+				})
+				.then(function (riders) {
+					vm.riders = riders;
+				});
+		}
+
+		vm.addForm = function () {
+			findRiders();
+			vm.competition = new competitionDataService.competitions();
+			vm.showForm = true;
+			vm.add = true;
+		};
+
+		vm.editForm = function (index) {
+			findRiders();
+			vm.competitionNoEdited = {};
+			vm.competition = new competitionDataService.competition();
+			for (var propertyName in vm.competitions[index]) {
+				vm.competition[propertyName] = vm.competitions[index][propertyName];
+			}
+			vm.competitionNoEdited = JSON.parse(JSON.stringify(vm.competition));
+			vm.showForm = true;
+			vm.add = false;
+		};
+
+		vm.saveCompetition = function () {
+			if (!vm.addCompetitionForm.$invalid) {
+				if (vm.add) {
+					vm.competition.$save();
+					vm.competitions.push(vm.competition);
+					vm.message = "Añadida competition: ";
+				}
+				vm.showMessage = true;
+				vm.showForm = false;
+			}
+		}
+
+		vm.editCompetition = function () {
+			if (!vm.addCompetitionForm.$invalid) {
+				findWinnersChange();
+				if (!vm.add) {
+					//vm.competition.$update();
+					vm.message = "Editada competición: ";
+					init();
+				}
+				vm.showMessage = true;
+				vm.showForm = false;
+			}
+		}
+
+		vm.removeCompetition = function (index) {
+			vm.competition = new competitionDataService.competition();
+			vm.competition._id = vm.competitions[index]._id;
+			vm.competition.$delete().then(function () {
+				vm.message = "Borrada competición: ";
+				vm.showMessage = true;
+				init();
+			});
+		}
 
 
-        vm.addForm = function () {
-            vm.competition = new competitionDataService.competitions();
-            vm.showForm = true;
-            vm.add = true;
-        };
-
-        vm.editForm = function (index) {
-            vm.competition = new competitionDataService.competition();
-            for (var propertyName in vm.competitions[index]) {
-                vm.competition[propertyName] = vm.competitions[index][propertyName];
-            }
-            vm.showForm = true;
-            vm.add = false;
-        };
-
-        vm.saveCompetition = function () {
-            if (!vm.addCompetitionForm.$invalid) {
-                if (vm.add) {
-                    vm.competition.$save();
-                    vm.competitions.push(vm.competition);
-                    vm.message = "Añadida competition: ";
-                }
-                vm.showMessage = true;
-                vm.showForm = false;
-            }
-        }
-
-        vm.editCompetition = function () {
-            if (!vm.addCompetitionForm.$invalid) {
-                if (!vm.add) {
-                    vm.competition.$update();
-                    vm.message = "Editada competición: ";
-                    init();
-                }
-                vm.showMessage = true;
-                vm.showForm = false;
-            }
-        }
-
-        vm.removeCompetition = function (index) {
-            vm.competition = new competitionDataService.competition();
-            vm.competition._id = vm.competitions[index]._id;
-            vm.competition.$delete().then(function () {
-                vm.message = "Borrada competición: ";
-                vm.showMessage = true;
-                init();
-            });
-        }
-
-    }
+	}
 })();
--- src/server/lib/routes/api/ridersApi.js
+++ src/server/lib/routes/api/ridersApi.js
@@ -6,96 +6,104 @@ var jwt = require('../../jwt.js');
 var convert = require('../../convert.js');
 
 var router = express.Router({
-    mergeParams: true
+	mergeParams: true
 });
 
 var schema = {
-    id: "ridersDetails",
-    type: "object",
-    properties: {
-        _id: {
-            type: "string"
-        },
-        safe_name: {
-            type: "string"
-        },
-        team: {
-            type: "string"
-        },
-        safe_name_team: {
-            type: "string"
-        },
-        country: {
-            type: "string"
-        }
-    },
-    required: ["_id", "safe_name", "team", "safe_name_team", "country"]
+	id: "ridersDetails",
+	type: "object",
+	properties: {
+		_id: {
+			type: "string"
+		},
+		safe_name: {
+			type: "string"
+		},
+		team: {
+			type: "string"
+		},
+		safe_name_team: {
+			type: "string"
+		},
+		country: {
+			type: "string"
+		}
+	},
+	required: ["_id", "safe_name", "team", "safe_name_team", "country"]
 }
 
 
 
 router.get('/keywords/:keywords', function (req, res) {
-        ridersData.findingByKeywords(req.params.keywords, parseInt(req.query.skip), parseInt(req.query.limit)).then(function (data) {
-            res.send(data);
-        }).fail(function (err) {
-            convert.resError(err, res);
-        });
-    })
-    .post('/add-season/:year', function (req, res) {
-        console.log(req.params.year);
-        ridersData.findingAllRiders().then(function (data) {
-            data.forEach(function (rider) {
-                var lastTeam = rider.team;
-                if (rider.seasons.length > 0) {
-                    lastTeam = rider.seasons[rider.seasons.length - 1].team;
-                }
-                if (rider.retired == true) {
-                    rider.seasons.push({
-                        year: req.params.year,
-                        team: lastTeam,
-                        palmares: []
-                    });
-                    ridersData.updatingRider(rider).fail(function (err) {
-                        console.log(err);
-                    });
-                }
-            });
-            competitionsData.findingAllCompetitions().then(function (competitions) {
-                competitions.forEach(function (competition) {
-                    if (!competition.seasons) {
-                        competition.seasons = [];
-                    }
-                    competition.seasons.push({
-                        year: req.params.year,
-                        numberStages: 0,
-                        stages: [],
-                        winner: "",
-                        mountain: "",
-                        regularity: ""
-                    });
-                    competitionsData.updatingCompetition(competition).fail(function (err) {
-                        console.log(err);
-                    });
-                });
-                res.send("data");
-            }).fail(function (err) {
-                convert.resError(err, res);
-            });
-        });
-    })
-    .put('/:rider', function (req, res) {
-        var tokenDecoded = jwt.decode(req.headers['x-access-token']);
-        jwt.verify(req, res);
-        ridersData.findingById(req.params.rider).then(function (data) {
-            res.send(data);
-            if (data)
-                convert.prom2res(ridersData.crud.updating(req.params.rider, req.body), res, 200);
-            else {
-                ridersData.crud.removing(req.params.rider);
-                convert.prom2res(ridersData.crud.inserting(req.body), res, 200);
-            }
-        });
-    });
+		ridersData.findingByKeywords(req.params.keywords, parseInt(req.query.skip), parseInt(req.query.limit)).then(function (data) {
+			res.send(data);
+		}).fail(function (err) {
+			convert.resError(err, res);
+		});
+	})
+	.post('/add-season/:year', function (req, res) {
+		console.log(req.params.year);
+		ridersData.findingAllRiders().then(function (data) {
+			data.forEach(function (rider) {
+				var lastTeam = rider.team;
+				if (rider.seasons.length > 0) {
+					lastTeam = rider.seasons[rider.seasons.length - 1].team;
+				}
+				if (rider.retired == true) {
+					rider.seasons.push({
+						year: parseInt(req.params.year),
+						team: lastTeam,
+						palmares: []
+					});
+					ridersData.updatingRider(rider).fail(function (err) {
+						console.log(err);
+					});
+				}
+			});
+			competitionsData.findingAllCompetitions().then(function (competitions) {
+				competitions.forEach(function (competition) {
+					if (!competition.seasons) {
+						competition.seasons = [];
+					}
+					var stagesList = [];
+					console.log(competition.numberStages);
+					for (var pos = 0; pos < competition.numberStages; pos++) {
+						console.log(pos);
+						stagesList.push({
+							description: "Etapa " + pos,
+							winner: "-"
+						});
+					}
+					competition.seasons.push({
+						year: parseInt(req.params.year),
+						stages: stagesList,
+						winner: "",
+						mountain: "",
+						regularity: ""
+					});
+					competitionsData.updatingCompetition(competition).fail(function (err) {
+						console.log(err);
+					});
+				});
+				res.send();
+			}).fail(function (err) {
+				convert.resError(err, res);
+			});
+		});
+	})
+	.put('/:rider', function (req, res) {
+		var tokenDecoded = jwt.decode(req.headers['x-access-token']);
+		jwt.verify(req, res);
+		ridersData.findingById(req.params.rider).then(function (data) {
+			res.send(data);
+			if (data)
+				convert.prom2res(ridersData.crud.updating(req.params.rider, req.body), res, 200);
+			else {
+				ridersData.crud.removing(req.params.rider);
+				convert.prom2res(ridersData.crud.inserting(req.body), res, 200);
+			}
+		});
+	});
 
 crudApi(router, ridersData, schema);
 
