2ddc1d3c25aca75c20d6cd4608f9e0968742a67d
antonioferngarcia <antonio.ferngarcia@gmail.com>
2015-11-27 11:11:42 +0100

    Fix maillots-url's for riders

--- src/client/app/components/teams/teams.js
+++ src/client/app/components/teams/teams.js
@@ -1,78 +1,76 @@
 "use strict";
 (function () {
-    angular
-        .module('teams', ['ui.router', 'riders', 'team', 'rider', 'riderCard'])
-        .config(config)
-        .directive('teams', directive)
-        .factory('teamsDataService', teamsDataService)
+	angular
+		.module('teams', ['ui.router', 'riders', 'team', 'rider', 'riderCard'])
+		.config(config)
+		.directive('teams', directive)
+		.factory('teamsDataService', teamsDataService)
 
-    function config($stateProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/',
-                template: '<teams></teams>'
-            })
-            .state('teams', {
-                url: '/equipos',
-                template: '<teams></teams>'
-            });
-    }
+	function config($stateProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				template: '<teams></teams>'
+			})
+			.state('teams', {
+				url: '/equipos',
+				template: '<teams></teams>'
+			});
+	}
 
-    function directive() {
-        return {
-            templateUrl: 'app/components/teams/teams.html',
-            controller: controller,
-            controllerAs: "vm",
-            bindToController: true
-        }
-    }
+	function directive() {
+		return {
+			templateUrl: 'app/components/teams/teams.html',
+			controller: controller,
+			controllerAs: "vm",
+			bindToController: true
+		}
+	}
 
-    function controller(teamsDataService, UtilService, settings) {
-        var vm = this;
+	function controller(teamsDataService, UtilService, settings) {
+		var vm = this;
 
-        function getTeams(status) {
-            return teamsDataService.gettingTeams({
-                limit: 100,
-                sort: '-_id',
-                q: 'status:' + status
-            });
-        }
+		function getTeams(status) {
+			return teamsDataService.gettingTeams({
+				limit: 100,
+				sort: '-_id',
+				q: 'status:' + status
+			});
+		}
 
-        vm.getMaillot = function (team) {
-            return "assets/images/teams_covers/" + vm.year + "/" + team + ".png"
-        };
+		vm.getMaillot = function (team) {
+			return "assets/images/teams_covers/" + vm.year + "/" + team + ".png"
+		};
 
 
-        init();
+		init();
 
-        function init() {
-            vm.year = settings.currentSeason;
-            getTeams('WT').then(function (teams) {
-                vm.teams_WT = teams;
-            });
-            getTeams('PC').then(function (teams) {
-                vm.teams_PCT = teams;
-            })
-        }
-    }
+		function init() {
+			vm.year = settings.currentSeason;
+			getTeams('WT').then(function (teams) {
+				vm.teams_WT = teams;
+			});
+			getTeams('PC').then(function (teams) {
+				vm.teams_PCT = teams;
+			})
+		}
+	}
 
-    function teamsDataService($resource) {
-        var factory = {};
+	function teamsDataService($resource) {
+		var factory = {};
 
-        factory.teams = $resource('api/teams', {});
+		factory.teams = $resource('api/teams', {});
 
-        factory.team = $resource('api/teams/:id', {
-            id: '@_id'
-        }, {
-            'update': {
-                method: 'PUT'
-            }
-        });
+		factory.team = $resource('api/teams/:id', {}, {
+			'update': {
+				method: 'PUT'
+			}
+		});
 
-        factory.gettingTeams = function (params) {
-            return factory.teams.query(params).$promise;
-        }
+		factory.gettingTeams = function (params) {
+			return factory.teams.query(params).$promise;
+		}
 
-        return factory;
-    }
+		return factory;
+	}
 })();
