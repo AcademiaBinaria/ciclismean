e83385d948410528fa9a899cfb70c4b3fcc5f66a
Marcos Muñiz García <esechinako@gmail.com>
2018-01-02 01:18:05 +0100

    Change season

--- src/client/app/components/teams/riders/rider/rider.js
+++ src/client/app/components/teams/riders/rider/rider.js
@@ -126,7 +126,7 @@
 		}
 
 		factory.getRiderImageUrl = function (rider) {
-			var year = settings.currentSeason;
+			var year = 2018;
 			var team = rider.safe_name_team;
 			var rider = rider.safe_name;
 			return "assets/images/riders_img/" + year + "/" + team + "/" + rider + ".jpg";
