f9427c790255f25cb4458cd5c24b79067ad55f0f
Marcos Muñiz García <esechinako@gmail.com>
2019-01-15 09:35:30 +0100

    Change base url

--- src/client/app/_common/app.js
+++ src/client/app/_common/app.js
@@ -3,14 +3,14 @@
 angular
     .module('template', ['ui.router', 'ui.bootstrap', 'ngStorage', 'main', 'navbar', 'footer', 'user', 'teams', 'competitions', 'util', 'dashboard', 'riderForm', 'teamForm', 'competitionForm', 'riderSeason', 'addSeason', 'loader'])
     .constant('settings', {
-        urlBase: 'http://localhost:3000/',
-        currentSeason: 2017
+        urlBase: 'http://ciclismania.es/',
+        currentSeason: 2019
     })
     .config(function ($httpProvider, settings, $sceDelegateProvider) {
         $sceDelegateProvider.resourceUrlWhitelist([
             "self",
-            "http://localhost:3000/**",
-            "http://46.101.149.113/**"
+            "http://ciclismania.es/**",
+            "http://46.101.149.113**"
         ]);
         $httpProvider.interceptors.push(function ($q) {
             return {
